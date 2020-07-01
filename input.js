7a57ec6b643855a57e57af1c3f9bba748932900d
Alyssa Beaton <ehseebe@gmail.com>
2020-07-01 19:28:36 +0000

    Connection input added

--- input.js
+++ input.js
@@ -1,16 +1,38 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+  connection = conn;
 
   const handleUserInput = function() {
     stdin.on('data', (key) => {
       if (key === '\u0003') {
         process.exit();
+      };
+
+      if (key === 'w') {
+        connection.write('Move: up')
+      };
+
+      if (key === 'a') {
+        connection.write('Move: left')
+      }
+
+      if (key === 's') {
+        connection.write('Move: down')
+      }
+
+      if (key === 'd') {
+        connection.write('Move: right')
       }
     })
+
+    
   }
+
   handleUserInput();
 
   return stdin;
