f74f47a1f4784cb718b6847c9a99ab9a91853449
Alyssa Beaton <ehseebe@gmail.com>
2020-07-01 19:00:52 +0000

    Refactored to move input functions to own file, input.js

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting...');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    })
-  }
-  handleUserInput();
-
-  return stdin;
-}
 
 
 
